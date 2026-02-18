289429dd3ecdeac598d1b3da2b9ea04dba425f63
Júlio Lobato <juliocesarlobato01@gmail.com>
2026-02-18 18:56:22 -0300

    criei uma controladora para adicionar, listar, deletar e alterar os departamentos.

---
+++ src/main/java/com/example/service_order_manager/repository/DepartamentoRepository.java
@@ -0,0 +1,7 @@
+package com.example.service_order_manager.repository;
+
+import com.example.service_order_manager.model.Departamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartamentoRepository extends JpaRepository<Departamento, Long> {
+}
